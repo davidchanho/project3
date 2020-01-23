bf59e10003d73b46dd4b8d1bf1c78e0eb860e66d
davidchanho <davidho.crd@gmail.com>
2020-01-23 14:31:40 -0500

    re add routes in app

--- client/src/App.js
+++ client/src/App.js
@@ -21,24 +21,7 @@ export default function App() {
   return (
     <Theme>
       <Template>
-        <Router>
-          <Switch>
-            <Route exact path='/' component={Trend} />
-            <Route exact path='/About' component={About} />
-            <Route
-              exact
-              path='/User'
-              render={props => <User {...props} user={user} />}
-            />
-            <Route exact path='/Sector' component={Trend} />
-            <Route exact path='/Watchlist' component={Watchlist} />
-            <Route exact path='/Login' component={Login} />
-            <Route exact path='/Logout' component={Logout} />
-            <Route exact path='/Signup' component={Signup} />
-            <Route exact path='/Trend' component={Trend} />
-            <Route component={PageNotFound} />
-          </Switch>
-        </Router>
+        <Routes user={user} />
       </Template>
     </Theme>
   )
