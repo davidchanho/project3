f4bd25041d68beb0bea54b78c226f72704200417
Patrick ONeill <poneill@sembracare.com>
2020-01-22 21:22:31 -0500

    updating get routes

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+app.use(routes);
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
