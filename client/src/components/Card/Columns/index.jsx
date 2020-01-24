56ff6b82453643e84a81aac215261e44ec71cb86
Logan Mc <logangmcnulty@gmail.com>
2020-01-24 17:50:01 -0500

    remove the conditional render in columns component for the circles to work

--- client/src/components/Card/Columns/index.jsx
+++ client/src/components/Card/Columns/index.jsx
@@ -1,22 +1,26 @@
 import React, { useState } from 'react'
 import CardColumns from 'react-bootstrap/CardColumns'
 import { TrendCard } from '../Card'
+
 export function Columns (props) {
-  console.log("PROPS YO")
-  console.log(props)
+  // console.log("PROPS YO")
+  // console.log(props)
   const { tests, onDelete, sectorHealthData } = props
   
   if(!sectorHealthData){
     return (null)
   }
 
+  console.log("SECTOR HEALTH DATA...")
   console.log(sectorHealthData)
   return (
-    <CardColumns>
-      {tests.map(test => (
-        <TrendCard key={test.id} test={test} onDelete={onDelete} />
-      ))}
-    </CardColumns>
+    <div>
+        <CardColumns>
+          {sectorHealthData &&tests.map(test => (
+            <TrendCard key={test.id} test={test} onDelete={onDelete} />
+          ))}
+      </CardColumns>
+    </div>
   )
 }
 // export const Columns = props => {
