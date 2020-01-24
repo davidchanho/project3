7512c9b262cd8897cb0a817ab77c8922137bcc95
Logan Mc <logangmcnulty@gmail.com>
2020-01-24 18:24:35 -0500

    temp merge

--- client/src/components/Card/Columns/index.jsx
+++ client/src/components/Card/Columns/index.jsx
@@ -6,11 +6,30 @@ export function Columns (props) {
   const {onDelete, sectorHealthData } = props
   console.log(sectorHealthData)
 
+  if(!sectorHealthData){
+    return(null)
+  }
+
   return (
-    <CardColumns>
-        {sectorHealthData.map(sector => (
-          <TrendCard key={sector.id} sector={sector} onDelete={onDelete} />
-        ))}
-    </CardColumns>
+    <div>
+        <CardColumns>{
+             sectorHealthData.map(sector=> (
+              <TrendCard key={sector.id} sector={sector} onDelete={onDelete} />
+            ))
+          }
+      </CardColumns>
+    </div>
   )
 }
+// export const Columns = props => {
+//   const { tests, onDelete } = props
+//   // console.log("PROPS YO")
+//   // console.log(props)
+//   return (
+//     <CardColumns>
+//       {tests.map(test => (
+//         <TrendCard key={test.id} test={test} onDelete={onDelete} />
+//       ))}
+//     </CardColumns>
+//   )
+// }
